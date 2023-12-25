49baef8d86ee584b6ec9873974380328815d69ca
آرش الطافي <0021328374@nahad.ir>
2023-12-25 11:46:53 +0330

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const Home = () => {
   return (
     <div className="flex flex-col gap-8">
@@ -8,6 +10,21 @@ const Home = () => {
       <p className="text-3xl text-center">
         نکست جی اس 14
       </p>
+
+      <ul className="flex flex-col gap-6 items-center justify-center mt-16">
+        <li className="hover:text-yellow-400">
+          <Link href="/about">About Page</Link>
+        </li>
+        <li className="hover:text-yellow-400">
+          <Link href="/contact">Contact Page</Link>
+        </li>
+        <li className="hover:text-yellow-400">
+          <Link href="/post">Post Page</Link>
+        </li>
+        <li className="hover:text-yellow-400">
+          <Link href="/post/123">Post 123 Page</Link>
+        </li>
+      </ul>
     </div>
   )
 }
