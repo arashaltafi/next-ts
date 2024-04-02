154ccfd32072c2269e843ad61978a09129c69d0c
arashaltafi <altafi.a@nahad.ir>
2024-04-02 14:03:20 +0330

    a

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -12,7 +12,14 @@ const index = () => {
   console.log('path:', path)
 
   return (
-    <div>path: {path}</div>
+    <div className="w-full h-[80vh] overflow-hidden flex flex-col items-center justify-center gap-8">
+      <h1 className="text-xl">path: {path}</h1>
+      <button
+        onClick={() => router.back()}
+        className="text-lg text-white bg-red-500 hover:bg-red-600 rounded-lg px-6 py-3 transition-all">
+        back
+      </button>
+    </div>
   )
 }
 
