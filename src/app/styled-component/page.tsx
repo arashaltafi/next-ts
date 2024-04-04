119cdd372ba187dc19403dd7a7726f672dc72e6c
Arash Altafi <arashaltafi1377@gmail.com>
2024-04-04 17:51:42 +0430

    Styled Component

--- src/app/styled-component/page.tsx
+++ src/app/styled-component/page.tsx
@@ -1,14 +1,39 @@
+'use client'
+
+import styled from 'styled-components';
+
 const page = () => {
+
+    const Main = styled.h1({
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    });
+
+    const Title1 = styled.h1({
+        color: 'red',
+    });
+
+    const Title2 = styled.h2({
+        color: 'blue',
+    });
+
+    const Title3 = styled.h3`
+        color: yellow;
+    `;
+
     return (
-        <div>
-            <h1>Test h1</h1>
-            <h2>Test h2</h2>
-            <h3>Test h3</h3>
+        <Main>
+            <Title1>Test h1</Title1>
+            <Title2>Test h2</Title2>
+            <Title3>Test h3</Title3>
             <h4>Test h4</h4>
             <h5>Test h5</h5>
             <h6>Test h6</h6>
             <p>Test P</p>
-        </div>
+        </Main>
     )
 }
 
