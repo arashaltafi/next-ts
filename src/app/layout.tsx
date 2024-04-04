386f8708fc126ab88c1dfd63a63cc9663d3d45df
Arash Altafi <arashaltafi1377@gmail.com>
2024-04-04 15:15:20 +0430

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'NextJs 14',
   description: 'Sample NextJs 14 app',
 }
+
+const vazirFont = localFont({
+  src: '../../public/fonts/Vazirmatn-Medium.ttf'
+})
 
 export default function RootLayout({
   children,
@@ -13,8 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`bg-slate-900 text-slate-200`}>
-        <main className='p-8'>
+      <body className={`bg-slate-900 text-slate-200 ${vazirFont.className}`}>
+        <main className='px-8 py-6'>
           {children}
         </main>
       </body>
--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,19 +1,21 @@
 "use client"
 
-import { usePathname } from "next/navigation";
-import { useRouter } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 
 const index = () => {
 
+  const params = useParams();
   const router = useRouter();
   const path = usePathname();
 
+  console.log('params:', params)
   console.log('router:', router)
   console.log('path:', path)
 
   return (
     <div className="w-full h-[80vh] overflow-hidden flex flex-col items-center justify-center gap-8">
       <h1 className="text-xl">path: {path}</h1>
+      <h1 className="text-xl">params: {params.id}</h1>
       <button
         onClick={() => router.back()}
         className="text-lg text-white bg-red-500 hover:bg-red-600 rounded-lg px-6 py-3 transition-all">
