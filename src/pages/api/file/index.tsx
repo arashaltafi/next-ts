676c4efe2c83b03c4c617f0315c994e29106c3a2
arashaltafi <altafi.a@nahad.ir>
2024-04-06 08:51:52 +0330

    writeFileSync

--- src/pages/api/file/index.tsx
+++ src/pages/api/file/index.tsx
@@ -3,16 +3,32 @@ import fs from 'fs'
 import path from 'path'
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-
     const pathName = path.join(process.cwd(), 'src/pages/api/file', 'user.json')
 
-    const data = fs.readFileSync(pathName);
-    const users = JSON.parse(data.toString()).users
-
     if (req.method == 'GET') {
+        const data = fs.readFileSync(pathName);
+        const users = JSON.parse(data.toString()).users
         res.status(200).send({ users: users })
     } else if (req.method == 'POST') {
-        res.status(201).send({ message: 'POST' })
+        const { id, name, family, age } = req.body;
+        if (!id || !name || !family || !age) {
+            return res.status(400).send({ message: 'Please Enter All Fields' })
+        } else {
+            const users = fs.readFileSync(pathName).toString();
+            const data = fs.writeFileSync(
+                pathName,
+                JSON.stringify(
+                    {
+                        users: [...JSON.parse(users).users,
+                        { id, name, family, age }]
+                    }
+                )
+            )
+    
+            console.log('data:', data)
+    
+            res.status(201).send({ message: data })
+        }
     } else {
         res.status(404).send({ message: 'Please Send Request With Method GET or POST' })
     }
