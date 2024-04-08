425da422ee311f32b36cadaff2613ce037ffdcc7
arashaltafi <altafi.a@nahad.ir>
2024-04-08 14:40:10 +0330

    a

--- src/app/emoji/page.tsx
+++ src/app/emoji/page.tsx
@@ -1,12 +1,47 @@
 "use client"
 
 import React from 'react'
-import EmojiPicker from 'emoji-picker-react';
+import EmojiPicker, { EmojiClickData, EmojiStyle, Theme } from 'emoji-picker-react';
 
 const EmojiPicker1 = () => {
     return (
         <div className='flex flex-col items-center justify-center gap-12 w-full'>
-            <EmojiPicker />
+            <EmojiPicker
+                searchDisabled={true}
+                skinTonesDisabled={true}
+                emojiStyle={EmojiStyle.NATIVE}
+                theme={Theme.AUTO}
+                height={450}
+                width={350}
+                reactionsDefaultOpen={true}
+                allowExpandReactions={false}
+                onEmojiClick={(emoji: EmojiClickData) => {
+                    console.log('onEmojiClick', emoji.emoji)
+                }}
+                onReactionClick={(emoji: EmojiClickData) => {
+                    console.log('onReactionClick', emoji.emoji)
+                }}
+            />
+
+
+            <EmojiPicker
+                className='mt-32'
+                searchDisabled={true}
+                skinTonesDisabled={true}
+                emojiStyle={EmojiStyle.NATIVE}
+                theme={Theme.AUTO}
+                height={450}
+                width={350}
+                previewConfig={{ showPreview: false }}
+                reactionsDefaultOpen={false}
+                allowExpandReactions={false}
+                onEmojiClick={(emoji: EmojiClickData) => {
+                    console.log('onEmojiClick', emoji.emoji)
+                }}
+                onReactionClick={(emoji: EmojiClickData) => {
+                    console.log('onReactionClick', emoji.emoji)
+                }}
+            />
         </div>
     )
 }
