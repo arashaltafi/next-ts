2447030187a7d69df2dca8056e95c7c807fed6c6
arashaltafi <altafi.a@nahad.ir>
2024-04-08 13:21:26 +0330

    loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div
+            className='text-center text-red-500 font-bold text-3xl'
+        >
+            loading
+        </div>
+    )
+}
+
+export default Loading
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import styles from "./styles/Home.module.css"
+import { Suspense } from 'react'
+import Loading from './loading'
 
 const Home = () => {
 
@@ -13,57 +15,62 @@ const Home = () => {
   }
 
   return (
-    <div className="flex flex-col gap-8">
-      <p className="text-3xl text-center">
-        Next Js 14
-      </p>
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col gap-8">
+        <p className="text-3xl text-center">
+          Next Js 14
+        </p>
 
-      <p className={`text-3xl text-center ${styles.title}`}>
-        نکست جی اس 14
-      </p>
+        <p className={`text-3xl text-center ${styles.title}`}>
+          نکست جی اس 14
+        </p>
 
-      <ul className="flex flex-col gap-6 items-center justify-center mt-16">
-        <li className="hover:text-yellow-400">
-          <Link href="/about">About Page</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/contact">Contact Page</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/styled-jsx">Styled Jsx</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/styled-component">Styled Component</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/bootstrap">Bootstrap</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/scss">Scss</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/use-server">{`use-server => Server Side Rendering (App Router)`}</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/use-client">{`use-client => Client Side Rendering (App Router)`}</Link>
-        </li>
-        <button onClick={handleClick} className="text-red-500 hover:text-blue-500">
-          Post Page
-        </button>
-        <li className="hover:text-yellow-400">
-          <Link href="/post/123">Post 123 Page</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/calendar">Calender</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/image">Image</Link>
-        </li>
-        <li className="hover:text-yellow-400">
-          <Link href="/emoji">EmojiPicker 1</Link>
-        </li>
-      </ul>
-    </div>
+        <ul className="flex flex-col gap-6 items-center justify-center mt-16">
+          <li className="hover:text-yellow-400">
+            <Link href="/about">About Page</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/contact">Contact Page</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/styled-jsx">Styled Jsx</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/styled-component">Styled Component</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/bootstrap">Bootstrap</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/scss">Scss</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/use-server">{`use-server => Server Side Rendering (App Router)`}</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/use-client">{`use-client => Client Side Rendering (App Router)`}</Link>
+          </li>
+          <button onClick={handleClick} className="text-red-500 hover:text-blue-500">
+            Post Page
+          </button>
+          <li className="hover:text-yellow-400">
+            <Link href="/post/123">Post 123 Page</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/calendar">Calender</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/image">Image</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <Link href="/emoji">EmojiPicker 1</Link>
+          </li>
+          <li className="hover:text-yellow-400">
+            <button onClick={() => router.push('/not-found')}>Not Found 1</button>
+          </li>
+        </ul>
+      </div>
+    </Suspense>
   )
 }
 
