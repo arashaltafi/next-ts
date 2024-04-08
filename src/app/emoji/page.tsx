6a3d109fc7d97c0e5ac0177811146c8d525c7873
Arash Altafi <arashaltafi1377@gmail.com>
2024-04-09 00:59:40 +0430

    a

--- src/app/emoji/page.tsx
+++ src/app/emoji/page.tsx
@@ -1,29 +1,48 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import EmojiPicker, { EmojiClickData, EmojiStyle, Theme } from 'emoji-picker-react';
 
 //https://www.npmjs.com/package/emoji-picker-react
 //https://www.npmjs.com/package/emoji-picker-element
 const EmojiPicker1 = () => {
+    const [isShowReaction, setIsShowReaction] = useState<boolean>(false);
+    const [reactionEmoji, setReactionEmoji] = useState<string>();
+
+    const handleClickToShowReaction = () => {
+        setIsShowReaction(!isShowReaction)
+    }
+
     return (
-        <div className='flex flex-col items-center justify-center gap-12 w-full'>
-            <EmojiPicker
-                searchDisabled={true}
-                skinTonesDisabled={true}
-                emojiStyle={EmojiStyle.NATIVE}
-                theme={Theme.AUTO}
-                height={450}
-                width={350}
-                reactionsDefaultOpen={true}
-                allowExpandReactions={false}
-                onEmojiClick={(emoji: EmojiClickData) => {
-                    console.log('onEmojiClick', emoji.emoji)
-                }}
-                onReactionClick={(emoji: EmojiClickData) => {
-                    console.log('onReactionClick', emoji.emoji)
-                }}
-            />
+        <div className='flex flex-col items-center justify-center gap-12 w-full select-none'>
+
+
+            <div
+                onClick={handleClickToShowReaction}
+                className='relative mt-16 bg-teal-500 hover:bg-teal-600 transition-all w-[90%] py-8 text-3xl text-white text-center rounded-3xl cursor-pointer'>
+                <p>
+                    click me!
+                </p>
+                <p>
+                    {reactionEmoji}
+                </p>
+                <div className={`absolute -top-6 -left-6 transition-all ${isShowReaction ? 'block' : 'hidden'}`}>
+                    <EmojiPicker
+                        searchDisabled={true}
+                        skinTonesDisabled={true}
+                        emojiStyle={EmojiStyle.NATIVE}
+                        theme={Theme.AUTO}
+                        height={450}
+                        width={350}
+                        reactionsDefaultOpen={true}
+                        allowExpandReactions={false}
+                        onReactionClick={(emoji: EmojiClickData) => {
+                            setReactionEmoji(emoji.emoji)
+                        }}
+                    />
+                </div>
+            </div>
+
 
 
             <EmojiPicker
