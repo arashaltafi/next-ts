b2d2e80e408c01d084ae50876c245fca9ebd26be
Arash Altafi <arashaltafi1377@gmail.com>
2024-04-15 01:26:17 +0430

    generateMetadata

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,17 +1,17 @@
-'use client'
-
-import { usePathname } from "next/navigation"
-
 const page = ({ params }: { params: { id: string } }) => {
   
-  const pathName = usePathname()
-  console.log('pathName:', pathName)
-
   return (
     <div>
         {`post id: ${params.id}`}
     </div>
   )
+}
+
+export const generateMetadata = ({ params, searchParams }: any) => {
+  return {
+    title: `Test title: ${params.id}`,
+    description: 'loadingSample description',
+  }
 }
 
 export default page
