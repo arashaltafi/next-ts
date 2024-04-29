5fb05cf7388c9138d128fdf7e5cf6e7ff7cb0de6
arashaltafi <altafi.a@nahad.ir>
2024-04-29 08:58:02 +0330

    fix ssr

---
+++ src/pages/ssr/[id].tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+type UserType = {
+  id: string,
+  title: number,
+  body: string
+}
+
+interface PropsType {
+  data: UserType
+}
+
+const SampleSSR = (props: PropsType) => {
+  return (
+    <div className='flex flex-col items-center justify-center gap-8 w-full px-16 py-8'>
+      <h1 className='font-bold text-3xl'>Sample SSR rendering in PageRouter</h1>
+      <p>server side rendering, create data in server for each run time (eact time)</p>
+      <p>its good for pages to have dynamic data for each user</p>
+      <h2 className='text-lg'><span className='text-green-500'>id:</span> {props.data.id}</h2>
+      <h2 className='text-lg'><span className='text-green-500'>title:</span> {props.data.title}</h2>
+      <h2 className='text-lg'><span className='text-green-500'>body:</span> {props.data.body}</h2>
+    </div>
+  )
+}
+
+export default SampleSSR
+
+export const getServerSideProps = async (context: any) => {
+  const { req, res, query, params } = context
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+
+  console.log('request:', req)
+  console.log('res:', res)
+  console.log('query:', query)
+  console.log('params:', params)
+
+  const data: UserType = await response.json();
+  console.log('SSR data:', data)
+
+  return {
+    props: { data }
+  };
+}
