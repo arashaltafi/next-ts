2b7f4b3dd40169c21845e69788a01377ac31cceb
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-27 13:40:34 +0430

    error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='flex items-center justify-center w-full h-screen'>
+            <h1 className='text-5xl text-red-500'>404 Custom Not Found Page!!!</h1>
+        </div>
+    )
+}
+
+export default NotFound
---
+++ src/pages/500.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServerError = () => {
+    return (
+        <div className='flex items-center justify-center w-full h-screen'>
+            <h1 className='text-5xl text-red-500'>ServerError Custom Page!!!</h1>
+        </div>
+    )
+}
+
+export default ServerError
