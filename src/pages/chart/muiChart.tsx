3f90c94e09c629c56a2c3d6c5c11a85d6f24dc4e
Arash Altafi <arashaltafi1377@gmail.com>
2024-06-07 18:56:06 +0430

    mui chart

---
+++ src/pages/chart/muiChart.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// https://mui.com/x/react-charts/
+const MuiChart = () => {
+    return (
+        <div className='w-full min-h-screen flex flex-col items-center justify-start gap-16'>
+            <h1 className='text-5xl'>MuiChart</h1>
+            <a className='text-sky-500' href="https://mui.com/x/react-charts/">Link Doc</a>
+        </div>
+    )
+}
+
+export default MuiChart
