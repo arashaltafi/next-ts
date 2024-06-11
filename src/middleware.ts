2b58024fc6f8b35ae0709ab4253a063c7746ec09
Arash Altafi <arashaltafi1377@gmail.com>
2024-06-11 12:39:58 +0330

    set limit for request

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+const rateLimitMap = new Map();
 
 export function middleware(request: NextRequest) {
     if (request.nextUrl.pathname.startsWith('/dashboard1')) {
@@ -8,6 +9,40 @@ export function middleware(request: NextRequest) {
         } else {
             return NextResponse.next()
         }
+    }
+
+    else if (request.nextUrl.pathname.startsWith('/api')) {
+        const ip = request.ip ?? "127.0.0.1";
+        const limit = 100;
+        const windowMs = 15 * 60 * 1000;
+        if (!rateLimitMap.has(ip)) {
+            rateLimitMap.set(ip, {
+                count: 0,
+                lastReset: Date.now(),
+            });
+        }
+        const ipData = rateLimitMap.get(ip);
+        if (Date.now() - ipData.lastReset > windowMs) {
+            ipData.count = 0;
+            ipData.lastReset = Date.now();
+        }
+
+        if (ipData.count >= limit) {
+            const data = {
+                success: false,
+                response: {
+                    message: "To Many Requests",
+                },
+            };
+            return new NextResponse(JSON.stringify(data), {
+                status: 429,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+
+        ipData.count += 1;
     }
 }
 
