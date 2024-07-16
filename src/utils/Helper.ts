15a82b6cc10611cee462744a2d1624a473083305
Arash Altafi <arashaltafi1377@gmail.com>
2024-07-16 16:57:37 +0430

    a

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -47,8 +47,19 @@ const delay = async (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+const base64ToBlob = (base64: string, contentType: string = 'application/pdf'): Blob => {
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: contentType });
+  };
+
 export {
     generateRandomNumber,
     convertMilliSecondToHoursMinute,
-    delay
+    delay,
+    base64ToBlob,
 }
