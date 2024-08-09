5fa2fdbd1365cafc385763801094792fe1b7dbcc
Arash Altafi <arashaltafi1377@gmail.com>
2024-08-09 21:40:39 +0430

    spline

---
+++ src/pages/spline/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline'
+
+//npm install @splinetool/react-spline
+// https://designcode.io/spline-react
+// https://spline.design/
+// https://app.spline.design/  (create online)
+const SplineSample = () => {
+    return (
+        <div className='w-full min-h-screen flex flex-col gap-16 items-center justify-start'>
+            <h1>Spline</h1>
+            <div className='w-2/3 flex flex-col gap-16'>
+                <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
+                <Spline scene="https://prod.spline.design/SDqBVKajWdVwV-uz/scene.splinecode" />
+                <Spline scene="https://prod.spline.design/4hEQkrrTZ6PyKf1C/scene.splinecode" />
+            </div>
+        </div>
+    )
+}
+
+export default SplineSample
