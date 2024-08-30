e85db1e5957fc8add4f1c02ae9b0c9a414b33de6
Arash Altafi <arashaltafi1377@gmail.com>
2024-08-30 21:09:53 +0430

    TextGsap

--- src/app/textGsap/page.tsx
+++ src/app/textGsap/page.tsx
@@ -12,17 +12,17 @@ const TextGsap = () => {
         const splitType = document.querySelector(".about-title")
         const text = new SplitType(splitType as HTMLElement, { types: "chars" })
         gsap.from(text.chars, {
-          scrollTrigger: {
-            trigger: splitType,
-            start: "top 80%",
-            end: "top 20%",
-            scrub: true,
-          },
-          duration: 1.8,
-          opacity: 0.2,
-          stagger: 0.5,
+            scrollTrigger: {
+                trigger: splitType,
+                start: "top 80%",
+                end: "top 20%",
+                scrub: true,
+            },
+            duration: 1.8,
+            opacity: 0.2,
+            stagger: 0.5,
         })
-      }, [])
+    }, [])
 
     return (
         <div className='w-full min-h-screen flex flex-col gap-32 items-center justify-start'>
@@ -30,16 +30,10 @@ const TextGsap = () => {
             <div className='h-80 w-full bg-red-500' />
 
             <div className="bg-black flex relative items-center justify-center">
-                <p className="about-title text-center font-thin lg:text-[4vw] sm:text-[5vw] text-[7.5vw] text-white inline">
-                    Hi I&apos;m <strong>Arash</strong> a <strong>fro</strong>ntend
-                    developer and <strong>JavaS</strong>cript enthusiast with a{" "}
-                    <strong>bach</strong>elor&apos;s <strong>degre</strong>e and over{" "}
-                    <strong>4 yea</strong>rs of
-                    <strong>expe</strong>rience. I&apos;ve <strong>succes</strong>sfully
-                    delivered <strong>hig</strong>h-quality user <strong>int</strong>
-                    erface <strong>soluti</strong>ons for <strong>star</strong>tups and{" "}
-                    <strong>enterp</strong>rise companies, <strong>gain</strong>ing
-                    valuable <strong>experie</strong>nce along the way.
+                <p className="about-title text-center text-lg text-white">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
                 </p>
             </div>
 
