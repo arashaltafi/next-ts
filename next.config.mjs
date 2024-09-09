0964726b95a58208fcc48306c6664c044afd9d80
Arash Altafi <arashaltafi1377@gmail.com>
2024-09-09 08:48:49 +0330

    handle persian route in rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,18 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      // {
+        // source: '/%D8%AF%D8%B1%D8%A8%D8%A7%D8%B1%D9%87-%D9%85%D8%A7', //درباره-ما
+        // destination: '/about',
+      // },
+      {
+        source: '/' + encodeURIComponent('درباره-ما'), //درباره-ما 
+        destination: '/about',
+      }
+    ];
+  },
   env: {
     GITHUB_APP_CLIENT_ID: 'Iv23liniCGdWWMhnaBIx',
     GITHUB_APP_CLIENT_SECRET: 'ebbXXXXXXX5ad0dXXXXXXXXXX9a940XXXXXXXa',
