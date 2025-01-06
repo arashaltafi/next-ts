639c7512ebde6c3b2c73d02c8e5d4d347dd8378d
Arash Altafi <arashaltafi1377@gmail.com>
2025-01-06 13:15:29 +0330

    a

--- src/app/api/captcha/route.ts
+++ src/app/api/captcha/route.ts
@@ -1,32 +1,32 @@
-import svgCaptcha from 'svg-captcha'
-import { Buffer } from 'buffer'
+// import svgCaptcha from 'svg-captcha'
+// import { Buffer } from 'buffer'
 
-export async function GET(req: Request, res: Response) {
-    try {
-        const options = {
-            size: 6, // Number of characters
-            ignoreChars: '0o1i', // Characters to ignore
-            noise: 5, // Number of noise lines
-            color: true, // Text color
-            background: '#ff0', // Background color
-            fontSize: 80, // Font size
-            width: 200, // Width
-            height: 60, // Height
-            mathOperator: '+/-', // Math operator
-            charPreset: '0123456789' // Character preset
-        };
-        const captcha = svgCaptcha.create(options);
-        const base64data = Buffer.from(captcha.data).toString('base64');
-        return Response.json({
-            code: captcha.text,
-            data: base64data
-        },
-            { status: 200 }
-        );
-    } catch (error) {
-        return Response.json(
-            { message: 'All fields are required' },
-            { status: 500 }
-        )
-    }
-}
+// export async function GET(req: Request, res: Response) {
+//     try {
+//         const options = {
+//             size: 6, // Number of characters
+//             ignoreChars: '0o1i', // Characters to ignore
+//             noise: 5, // Number of noise lines
+//             color: true, // Text color
+//             background: '#ff0', // Background color
+//             fontSize: 80, // Font size
+//             width: 200, // Width
+//             height: 60, // Height
+//             mathOperator: '+/-', // Math operator
+//             charPreset: '0123456789' // Character preset
+//         };
+//         const captcha = svgCaptcha.create(options);
+//         const base64data = Buffer.from(captcha.data).toString('base64');
+//         return Response.json({
+//             code: captcha.text,
+//             data: base64data
+//         },
+//             { status: 200 }
+//         );
+//     } catch (error) {
+//         return Response.json(
+//             { message: 'All fields are required' },
+//             { status: 500 }
+//         )
+//     }
+// }
