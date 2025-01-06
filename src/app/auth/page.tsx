dec77159a119f8df4b7c02606c8a6e6acef1fe72
Arash Altafi <arashaltafi1377@gmail.com>
2025-01-06 12:09:12 +0330

    a

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,34 +1,34 @@
 "use client";
 
 import React from 'react'
-import { signIn, signOut, useSession } from "next-auth/react";
+// import { signIn, signOut, useSession } from "next-auth/react";
 
 const GithubAuth = () => {
-    const { data: session, status } = useSession();
+    // const { data: session, status } = useSession();
 
-    if (session) {
-        return (
-            <>
-                <h1>GithubAuth</h1>
-                <h2>{session?.user?.name}</h2>
-                <br />
-                ClientComponent {status}{' '}
-                {status === 'authenticated' && session.user?.name}
-                <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
-        )
-    } else {
+    // if (session) {
+    //     return (
+    //         <>
+    //             <h1>GithubAuth</h1>
+    //             <h2>{session?.user?.name}</h2>
+    //             <br />
+    //             ClientComponent {status}{' '}
+    //             {status === 'authenticated' && session.user?.name}
+    //             <br />
+    //             {/* <button onClick={() => signOut()}>Sign out</button> */}
+    //         </>
+    //     )
+    // } else {
         return (
             <>
                 <h1>GithubAuth</h1>
                 <h2>Not signed in</h2>
                 <br />
-                ClientComponent {status}{' '}
-                <button onClick={() => signIn()}>Sign in</button>
+                {/* ClientComponent {status}{' '} */}
+                {/* <button onClick={() => signIn()}>Sign in</button> */}
             </>
         )
-    }
+    // }
 }
 
 export default GithubAuth
